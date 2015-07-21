1e59da007818593396a3cb7f2ad9b3581e5d8065
Muresan Ionut <muresan.ionut95@gmail.com>
2015-07-21 11:34:56 +0300

    login with admin just admin field is true

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def authenticate_mentor_user!
+    authenticate_user!
+    pp '11111111111111111111111111111111111111111111'
+    pp current_user
+    pp '11111111111111111111111111111111111111111111'
+
+    fail SecurityError if current_user.admin == false
+  end
+
+  rescue_from SecurityError do |_exception|
+    redirect_to '/admin/logout'
+  end
+
   def render_response body, code=200
     body = {
       code: code,
