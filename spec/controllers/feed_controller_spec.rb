87f052a8da4b83ac59c8e6f74d627f753df1dfe1
Muresan Ionut <muresan.ionut95@gmail.com>
2015-08-27 18:00:40 +0300

    change tests for FeedContorller use factories and let

--- spec/controllers/feed_controller_spec.rb
+++ spec/controllers/feed_controller_spec.rb
@@ -2,40 +2,36 @@ require 'rails_helper'
 
 RSpec.describe FeedController, type: :controller do
   render_views
-
+  let(:feed) { create :feed }
   describe 'GET' do
 
     it 'index' do
-
       get :index, format: :json
       expect(json[:body].count).to eq(0)
 
-      game = Game.create!(name: 'LoL', category: 'STRATEGY')
-      user = User.create!(email: 'ionut@example.com', username: 'Ionut', password: 'password', password_confirmation: 'password')
-      feed = Feed.create!(game_id: game.id, user_id: user.id, hours: 10, minutes: 10)
+      user = create :user
+      create :feed, user: user
 
       get :index, format: :json
       expect(json[:body].count).to eq(1)
 
-      game = Game.create!(name: 'Battlefield', category: 'FPS')
-      feed = Feed.create!(game_id: game.id, user_id: user.id, hours: 11, minutes: 10)
+      create :feed, user: user
 
       get :index, format: :json
       expect(json[:body].count).to eq(2)
-
     end
 
     it 'show when feet exist' do
-      game = Game.create!(name: 'LoL', category: 'STRATEGY')
-      user = User.create!(email: 'ionut@example.com', username: 'Ionut', password: 'password', password_confirmation: 'password')
-      feed = Feed.create!(game_id: game.id, user_id: user.id, hours: 10, minutes: 10)
-
+      feed = create :feed
       get :show, id: feed.id, format: :json
       expect(json[:body][:id].to_s).to eq(feed.id.to_s)
+      expect(json[:body][:game_id].to_s).to eq(feed.game_id.to_s)
+      expect(json[:body][:user_id].to_s).to eq(feed.user_id.to_s)
+      expect(json[:body][:hours].to_s).to eq(feed.hours.to_s)
     end
 
     it 'show when feet does not exist' do
-      get :show, id: 11, format: :json
+      get :show, id: -1, format: :json
       expect(json[:code]).to eq(400_031)
       expect(json[:body]).to eq('Feed does not exist!')
     end
@@ -43,11 +39,10 @@ RSpec.describe FeedController, type: :controller do
   end
 
   describe 'POST' do
-
     it 'create when user and game exist' do
-      game = Game.create!(name: 'LoL', category: 'STRATEGY')
-      user = User.create!(email: 'ionut@example.com', username: 'Ionut', password: 'password', password_confirmation: 'password')
-      key = Key.create!(user_id: user.id, consumer_key: SecureRandom.base64(20), secret_key: SecureRandom.base64(20) )
+      game = create :game
+      user = create :user
+      key = create :key, user: user
 
       post :create, consumer_key: key.consumer_key, secret_key: key.secret_key, feed: {game_id: game.id, user_id: user.id, hours: 2, minutes: 15}, format: :json
 
@@ -67,11 +62,9 @@ RSpec.describe FeedController, type: :controller do
     end
 
     it 'create when user or game does not exist' do
-
-      game = Game .create(name: "LoL", category: "STRATEGY")
-      user = User.create!(email: 'ionut@example.com', username: 'Ionut', password: 'password', password_confirmation: 'password')
-      key = Key.create!(user_id: user.id, consumer_key: SecureRandom.base64(20), secret_key: SecureRandom.base64(20) )
-
+      game = create :game
+      user = create :user
+      key = create :key, user: user
 
       post :create, consumer_key: key.consumer_key, secret_key: key.secret_key, feed: {game_id: game.id, user_id: 12, hours: 2, minutes: 15}, format: :json
       expect(json[:code]).to eq(400_032)
@@ -80,45 +73,35 @@ RSpec.describe FeedController, type: :controller do
       post :create, consumer_key: key.consumer_key, secret_key: key.secret_key, feed: {game_id: 12, user_id: user.id, hours: 2, minutes: 15}, format: :json
       expect(json[:code]).to eq(400_032)
       expect(json[:body]).to eq("Can't create feed!")
-
-
     end
 
     it 'create when user or game does not exist' do
-
-      user = User.create!(email: 'ionut@example.com', username: 'Ionut', password: 'password', password_confirmation: 'password')
-      game = Game .create(name: "LoL", category: "STRATEGY")
-      key = Key.create!(user_id: user.id, consumer_key: SecureRandom.base64(20), secret_key: SecureRandom.base64(20) )
+      game = create :game
+      user = create :user
+      key = create :key, user: user
 
       post :create, consumer_key: key.consumer_key, secret_key: key.secret_key, feed: {game_id: game.id, user_id: 12, hours: 2, minutes: 15}, format: :json
       expect(json[:code]).to eq(400_032)
       expect(json[:body]).to eq("Can't create feed!")
-
     end
 
     it 'create when time is not integer or does not exist' do
-
-      game = Game .create(name: "LoL", category: "STRATEGY")
-      user = User.create!(email: 'ionut@example.com', username: 'Ionut', password: 'password', password_confirmation: 'password')
-      key = Key.create!(user_id: user.id, consumer_key: SecureRandom.base64(20), secret_key: SecureRandom.base64(20) )
+      game = create :game
+      user = create :user
+      key = create :key, user: user
 
       post :create, consumer_key: key.consumer_key, secret_key: key.secret_key, feed: {game_id: game.id, user_id: user.id, hours: 'asd', minutes: '21sd'}, format: :json
       expect(json[:code]).to eq(400_032)
       expect(json[:body]).to eq("Can't create feed!")
-
     end
 
     it 'create when user is not login' do
-
-      game = Game .create(name: "LoL", category: "STRATEGY")
-      user = User.create!(email: 'ionut@example.com', username: 'Ionut', password: 'password', password_confirmation: 'password')
+      game = create :game
+      user = create :user
 
       post :create, consumer_key: 'test', secret_key: 'test', feed: {game_id: game.id, user_id: user.id, hours: 'asd', minutes: '21sd'}, format: :json
       expect(json[:code]).to eq(400_011)
       expect(json[:body]).to eq("User is not logged")
-
     end
-
   end
-
 end
