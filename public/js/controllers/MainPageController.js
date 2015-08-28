505862f9954cc9e153eb523a56b4ac70d6939c3a
Muresan Ionut <muresan.ionut95@gmail.com>
2015-08-28 15:42:39 +0300

    create form for controller

--- public/js/controllers/MainPageController.js
+++ public/js/controllers/MainPageController.js
@@ -8,6 +8,7 @@ app.controller('MainPageController', function ($scope, $http) {
 	$scope.users = {};
 	$scope.number_of_users = 0;
 	$scope.feeds = {};
+	$scope.myMsg = '';
 
 	$http.get('/api/games.json').success(function(data){
 		$scope.games = data['body'];
@@ -27,12 +28,19 @@ app.controller('MainPageController', function ($scope, $http) {
 		$scope.number_of_users = $scope.users.length;
 	});
 
-
 	$scope.logout = function(){
 		Cookies.remove('user_id');
 		Cookies.remove('consumer_key');
 		Cookies.remove('secret_key');
 		window.location.replace("/#/index");
 	};
+	var textarea = document.getElementById('comment');
 
+	$scope.chat_text = '';
+
+  $scope.sendMessage = function() {
+		$scope.chat_text = $scope.chat_text + $scope.myMsg + '\n';
+  	$scope.myMsg = '';
+		textarea.scrollTop = textarea.scrollHeight;
+	}
 });
